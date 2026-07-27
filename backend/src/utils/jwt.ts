2ab291f1e1ea649546a9f65c54bbe6947630ca55
Maherun Nessa <meherunela2002@gmail.com>
2026-07-27 23:41:41 +0600

    chore: configure authentication utilities

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,29 @@
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+import { env } from "../config/env.js";
+
+interface TokenPayload {
+  userId: string;
+  email: string;
+}
+
+export const generateAccessToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, env.JWT_ACCESS_SECRET, {
+    expiresIn: env.JWT_ACCESS_EXPIRES_IN,
+  } as SignOptions);
+};
+
+export const generateRefreshToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, env.JWT_REFRESH_SECRET, {
+    expiresIn: env.JWT_REFRESH_EXPIRES_IN,
+  } as SignOptions);
+};
+
+export const verifyAccessToken = (token: string): TokenPayload | JwtPayload => {
+  return jwt.verify(token, env.JWT_ACCESS_SECRET) as TokenPayload | JwtPayload;
+};
+
+export const verifyRefreshToken = (
+  token: string,
+): TokenPayload | JwtPayload => {
+  return jwt.verify(token, env.JWT_REFRESH_SECRET) as TokenPayload | JwtPayload;
+};
---
+++ backend/src/utils/password.ts
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+import { env } from "../config/env.js";
+
+export const hashPassword = async (password: string): Promise<string> => {
+  return await bcrypt.hash(password, env.BCRYPT_SALT_ROUNDS);
+};
+
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string,
+): Promise<boolean> => {
+  return await bcrypt.compare(password, hashedPassword);
+};
